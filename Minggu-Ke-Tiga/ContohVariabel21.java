4def85690857b778342cdd4288866ac2da8b5876
ezreals-ares <nawaf303@gmail.com>
2024-09-06 22:47:10 +0700

    Merubah isi variable Percobaan 1

--- Minggu-Ke-Tiga/ContohVariabel21.java
+++ Minggu-Ke-Tiga/ContohVariabel21.java
@@ -1,15 +1,15 @@
 public class ContohVariabel21 {
 
     public static void main(String[] args) {
-        String salahSatuHobySayaAdalah = "Bermain petak umpet";
-        boolean ispandai = true;
-        char jenisKelamin = 'L';
-        byte _umurSayaSekarang = 20;
-        double $ipk = 3.24, tinggi = 1.78;
-        System.out.println(salahSatuHobySayaAdalah);
-        System.out.println("Apakah pandai? " + ispandai);
-        System.out.println("Jenis kelamin: " + jenisKelamin);
-        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        String Hobi_saya_adalah = "Hobi Saya Membaca Novel";
+        boolean Penjumlahan = true;
+        char Jenis_kelamin_saya = 'L';
+        byte Umur_saya_sekarang = 19;
+        double $ipk = 0.00, tinggi = 168.0;
+        System.out.println(Hobi_saya_adalah);
+        System.out.println("Apakah 2 + 2 = 4? " + Penjumlahan);
+        System.out.println("Jenis kelamin saya adalah : " + Jenis_kelamin_saya);
+        System.out.println("Umurku saat ini adalah : " + Umur_saya_sekarang);
         System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
 
     }
