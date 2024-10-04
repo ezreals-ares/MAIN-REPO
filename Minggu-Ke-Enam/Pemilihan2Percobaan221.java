023ae0d4912c17fecddf87c655696f2484f8df7a
ezreals-ares <nawaf303@gmail.com>
2024-10-04 07:18:50 +0700

    Memodifikasi Program

---
+++ Minggu-Ke-Enam/Pemilihan2Percobaan221.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan221 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        int pilihan_menu;
+        String member;
+        System.out.println("-------------------");
+        System.out.println("===MENU KAFE JTI===");
+        System.out.println("-------------------");
+        System.out.println("1. Ricebowl");
+        System.out.println("2. Ice Tea");
+        System.out.println("3. Paket Bunding (Ricebowl + Ice Tea)");
+        System.out.println("----------------------------------------");
+        System.out.println("Masukkan angka dari menu yang dipilih : ");
+        pilihan_menu = input21.nextInt();
+        System.out.println("apakah punya member (y/n) ? : ");
+        member = input21.nextLine();
+        System.out.println("----------------------------------------");
+        
+
+    }
+    
+}
