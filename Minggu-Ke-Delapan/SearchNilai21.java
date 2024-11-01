61c1fc93a3bf4600ae3682e2d18195eeea6fa8df
ezreals-ares <nawaf303@gmail.com>
2024-11-01 09:50:39 +0700

    Memodifikasi kode program

--- Minggu-Ke-Delapan/SearchNilai21.java
+++ Minggu-Ke-Delapan/SearchNilai21.java
@@ -1,19 +1,42 @@
+import java.util.Scanner;
+
 public class SearchNilai21 {
 
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int jmlhMhs;
+        int key, hasil = 0;
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
-            }
+        System.out.print("Masukkan banyaknya nilai yang akan di input : ");
+        jmlhMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlhMhs];
+       
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        System.out.print("Masukkan nilai yang akan dicari : ");
+        key = sc.nextInt();
+
+       for (int i = 0; i < nilaiMhs.length; i++) {
+            if (key == nilaiMhs[i]) {
+                hasil = i + 1;
+                break;
+            }      
+       }
+       if (hasil > 0) {
+            System.out.println();
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+            System.out.println();       
+
+       } else {
+            System.out.println();
+            System.out.println("Nilai tidak ditemukan");
+            System.out.println();       
+
+       }
+       
+        sc.close();
     }
 }
