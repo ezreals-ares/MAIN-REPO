bae60ef2f66bceeaad3f3a3c612d6d05a242ea54
ezreals-ares <nawaf303@gmail.com>
2024-11-03 11:08:37 +0700

    Memodifikasi kode program

--- Minggu-Ke-Delapan/SearchNilai21.java
+++ Minggu-Ke-Delapan/SearchNilai21.java
@@ -5,7 +5,8 @@ public class SearchNilai21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int jmlhMhs;
-        int key, hasil = 0;
+        int key;
+        boolean ditemukan = false;
 
         System.out.print("Masukkan banyaknya nilai yang akan di input : ");
         jmlhMhs = sc.nextInt();
@@ -16,26 +17,20 @@ public class SearchNilai21 {
             nilaiMhs[i] = sc.nextInt();
         }
 
-        System.out.print("Masukkan nilai yang akan dicari : ");
+        System.out.print("\nMasukkan nilai yang akan dicari : ");
         key = sc.nextInt();
 
        for (int i = 0; i < nilaiMhs.length; i++) {
             if (key == nilaiMhs[i]) {
-                hasil = i + 1;
-                break;
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (i + 1));
+                ditemukan = true;
             }      
        }
-       if (hasil > 0) {
-            System.out.println();
-            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
-            System.out.println();       
 
-       } else {
-            System.out.println();
-            System.out.println("Nilai tidak ditemukan");
-            System.out.println();       
-
+       if (!ditemukan) {
+            System.out.println("Nilai " + key + " tidak ditemukan");
        }
+       
        
         sc.close();
     }
