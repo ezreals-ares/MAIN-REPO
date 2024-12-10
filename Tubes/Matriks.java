2d88a6db0a369235714a2a1e12a5c299a1935c91
ezreals-ares <nawafazril303@gmail.com>
2024-12-10 08:21:52 +0700

    Kurang invers

---
+++ Tubes/Matriks.java
@@ -0,0 +1,342 @@
+package Tubes;
+
+import java.util.Scanner;
+
+public class Matriks {
+
+    static int baris, kolom;
+    static int i, j, k;
+    static Scanner sc = new Scanner(System.in);
+
+    static void tampilMenu() {
+        System.out.println("\n|===================================|");
+        System.out.println("|========= OPERASI MATRIKS =========|");
+        System.out.println("|===================================|");
+        System.out.println("| 1. Operasi (+) dan (-)            |");
+        System.out.println("| 2. Operasi Kali (x) antar Matriks |");
+        System.out.println("| 3. Menentukan Invers Matriks      |");
+        System.out.println("| 4. Menentukan Transpose Matriks   |");
+        System.out.println("| 5. keluar                         |");
+        System.out.println("|===================================|\n");
+    }
+
+    static void operasiTambahKurang() {
+        int pilihanTambahKurang;
+
+        do {
+            System.out.println("\n== OPERASI (+) DAN (-) ==");
+            System.out.println("1. Operasi Tambah");
+            System.out.println("2. Operasi Kurang");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihanTambahKurang = sc.nextInt();
+            switch (pilihanTambahKurang) {
+                case 1:
+                    operasiTambah();
+                    break;
+                case 2:
+                    operasiKurang();
+                    break;
+                case 3:
+                    System.out.println("Kembali Ke Menu Utama");
+                    return;
+                default:
+                    System.out.println("Pilihan Tidak Valid, Coba Lagi!");
+                    break;
+            }
+
+        } while (pilihanTambahKurang != 3);
+    }
+
+    static void operasiTambah() {
+        boolean valid = false;
+        double[][] matriks_1;
+        double[][] matriks_2;
+        double[][] result;
+
+        do {
+            System.out.print("Masukkan jumlah Baris untuk kedua Matriks: ");
+            baris = sc.nextInt();
+            System.out.print("Masukkan jumlah Kolom untuk kedua Matriks: ");
+            kolom = sc.nextInt();
+
+            if (baris <= 0 || kolom <= 0) {
+                System.out.println("Matriks tidak valid, coba lagi!");
+                continue;
+            }
+
+            matriks_1 = new double[baris][kolom];
+            matriks_2 = new double[baris][kolom];
+            result = new double[baris][kolom];
+
+            System.out.print("Masukkan elemen matriks pertama: \n");
+            inputElemenMatriks(matriks_1, baris, kolom);
+
+            System.out.print("Masukkan elemen matriks kedua: \n");
+            inputElemenMatriks(matriks_2, baris, kolom);
+
+            for (i = 0; i < baris; i++) {
+                for (j = 0; j < kolom; j++) {
+                    result[i][j] = matriks_1[i][j] + matriks_2[i][j];
+                }
+            }
+
+            System.out.println("\nHasil operasi tambah\n");
+            tampilMatriks(result, baris, kolom);
+            valid = true;
+
+        } while (!valid);
+
+    }
+
+    static void operasiKurang() {
+        boolean valid = false;
+        double[][] matriks_1;
+        double[][] matriks_2; 
+        double[][] result;
+        do {
+            System.out.print("Masukkan jumlah Baris untuk kedua Matriks: ");
+            baris = sc.nextInt();
+            System.out.print("Masukkan jumlah Kolom untuk kedua Matriks: ");
+            kolom = sc.nextInt();
+
+            if (baris <= 0 || kolom <= 0) {
+                System.out.println("Matriks tidak valid, coba lagi!");
+                continue;
+            }
+
+            matriks_1 = new double[baris][kolom];
+            matriks_2 = new double[baris][kolom];
+            result = new double[baris][kolom];
+
+            System.out.print("Masukkan elemen matriks pertama: \n");
+            inputElemenMatriks(matriks_1, baris, kolom);
+
+            System.out.print("Masukkan elemen matriks kedua: \n");
+            inputElemenMatriks(matriks_2, baris, kolom);
+
+            for (i = 0; i < baris; i++) {
+                for (j = 0; j < kolom; j++) {
+                    result[i][j] = matriks_1[i][j] - matriks_2[i][j];
+                }
+            }
+
+            System.out.println("\nHasil operasi kurang\n");
+            tampilMatriks(result, baris, kolom);
+            valid = true;
+
+        } while (!valid);
+    }
+
+    static void operasiPerkalian() {
+        boolean valid = false;
+        int baris1, baris2, kolom1, kolom2;
+        double[][] matriks1;
+        double[][] matriks2;
+        double[][] result;
+
+        do {
+            System.out.print("Masukkan baris matriks pertama: ");
+            baris1 = sc.nextInt();
+            System.out.print("Masukkan kolom matriks pertama: ");
+            kolom1 = sc.nextInt();
+            System.out.print("Masukkan baris matriks kedua: ");
+            baris2 = sc.nextInt();
+            System.out.print("Masukkan kolom matriks kedua: ");
+            kolom2 = sc.nextInt();
+
+            if (baris1 <= 0 || kolom1 <= 0 || baris2 <= 0 || kolom2 <= 0) {
+                System.out.println("Matriks tidak valid, coba lagi!");
+                continue;
+            }
+            if (kolom1 != baris2) {
+                System.out.println("Matriks tidak dapat dikalikan, coba lagi!");
+                continue;
+            }
+
+            System.out.println("Masukkan elemen matriks pertama: ");
+            matriks1 = new double[baris1][kolom1];
+            inputElemenMatriks(matriks1, baris1, kolom1);
+
+            System.out.println("Masukkan elemen matriks kedua: ");
+            matriks2 = new double[baris2][kolom2];   
+            inputElemenMatriks(matriks2, baris2, kolom2);
+
+            result = new double[baris1][kolom2];
+            for (i = 0; i < baris1; i++) {
+                for (j = 0; j < kolom2; j++) {
+                    for (k = 0; k < baris2; k++) {
+                        result[i][j] += matriks1[i][k] * matriks2[k][j];
+                    }
+                }
+            }
+
+            System.out.println("\nHasil perkalian matriks: ");
+            tampilMatriks(result, baris1, kolom2);
+
+            valid = true;
+
+        } while (!valid);
+
+    }
+
+    static void transposeMatriks() {
+        double[][] matriksAwal_Ts;
+        double[][] transpose;
+        boolean valid = false;
+
+        do{
+            System.out.print("Masukkan jumlah Baris Matriks: ");
+            baris = sc.nextInt();
+            System.out.print("Masukkan jumlah Kolom Matriks: ");
+            kolom = sc.nextInt();
+
+            if (baris <= 0 || kolom <= 0) {
+                System.out.println("Matriks tidak valid, coba lagi!");
+                continue;
+            }
+
+            matriksAwal_Ts = new double[baris][kolom];
+            inputElemenMatriks(matriksAwal_Ts, baris, kolom);
+
+            transpose = hitungTranspose(matriksAwal_Ts);
+
+            System.out.println("\nMatriks Asli: \n");
+            tampilMatriks(matriksAwal_Ts, baris, kolom);
+
+            System.out.println("\nTranspose Matriks: \n");
+            tampilMatriks(transpose, kolom, baris);
+
+            valid = true;
+
+        }while(!valid);
+
+        
+    }
+
+    static double[][] hitungTranspose(double[][] matriks) {
+        double[][] hasilTranspose = new double[kolom][baris];
+        for (i = 0; i < baris; i++) {
+            for (j = 0; j < kolom; j++) {
+                hasilTranspose[j][i] = matriks[i][j];
+            }
+        }
+        return hasilTranspose;
+    }
+
+    static void inversMatriks() {
+        int pilihanivers;
+        do {
+            System.out.println("\n== INVERS MATRIKS ==");
+            System.out.println("1. Invers 2x2");
+            System.out.println("2. Invers 3x3");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihanivers = sc.nextInt();
+
+            switch (pilihanivers) {
+                case 1:
+                    inversOrdo2();
+                    break;
+                case 2:
+                        
+                    break;
+                case 3:
+                    System.out.println("Kembali ke menu utama");
+                    return;
+                default:
+                    System.out.println("Pilihan tidak valid, coba lagi!");
+                    break;
+            }
+
+        } while (pilihanivers != 3);
+    }
+
+    static void inversOrdo2() {
+        boolean valid = false;
+        double[][] matriksAwal_inv;
+        baris = 2;
+        kolom = 2;
+        do {
+            System.out.println("\nMasukkan Elemen-Elemen Matriks: ");
+            matriksAwal_inv = new double[baris][kolom];
+            inputElemenMatriks(matriksAwal_inv, baris, kolom);
+
+            double determinant = matriksAwal_inv[0][0] * matriksAwal_inv[1][1] - matriksAwal_inv[0][1] * matriksAwal_inv[1][0];
+
+            if (determinant != 0) {
+                double[][] inverse = new double[2][2];
+                inverse[0][0] = matriksAwal_inv[1][1] / determinant;
+                inverse[0][1] = -matriksAwal_inv[0][1] / determinant;
+                inverse[1][0] = -matriksAwal_inv[1][0] / determinant;
+                inverse[1][1] = matriksAwal_inv[0][0] / determinant;
+
+                System.out.println("\nHasil Invers Matriks\n");
+                tampilMatriks(inverse, baris, kolom);
+
+                valid = true;
+
+            } else {
+                System.out.println("Matriks tidak memiliki invers");
+                continue;
+            }
+
+        } while (!valid);
+
+    }
+
+    static void tampilMatriks(double[][] matriks, int baris, int kolom) {
+        for (i = 0; i < baris; i++) {
+            System.out.printf("| \t\t");
+            for (j = 0; j < kolom; j++) {
+                System.out.printf("%s \t", matriks[i][j]);
+            }
+            System.out.println("\t\s|");
+        }
+    }
+
+    static void inputElemenMatriks(double[][] matriks, int baris, int kolom) {
+        for (i = 0; i < baris; i++) {   
+            for (j = 0; j < kolom; j++) {
+                System.out.print("Elemen [" + (i + 1) + "][" + (j + 1) + "]: ");
+                matriks[i][j] = sc.nextDouble();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int pilihan;
+        do {
+            tampilMenu();
+            System.out.print("Masukkan Pilihan: ");
+            pilihan = sc.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    operasiTambahKurang();
+                    break;
+
+                case 2:
+                    operasiPerkalian();
+                    break;
+
+                case 3:
+                    inversMatriks();
+                    break;
+
+                case 4:
+                    transposeMatriks();
+                    break;
+
+                case 5:
+                    System.out.println("Keluar");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid, coba lagi!");
+                    break;
+            }
+
+        } while (pilihan != 5);
+    }
+}
